b29cbd91c8416afcfafc817aaabd9f6c1ebb734e
Ryuki Goto <rth510ryu@gmail.com>
2021-04-27 21:51:16 +0900

    タイトルの変更

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,7 +6,9 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>
+          reactの練習
+        </title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
