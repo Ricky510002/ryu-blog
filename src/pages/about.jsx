9f952343cef966ae300edc6073acec1c6f5aeaed
Ryuki Goto <rth510ryu@gmail.com>
2021-04-28 17:01:18 +0900

    headの追加

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,7 +1,16 @@
-
+import Head from "next/head";
 
 export default function about(){
   return(
+    <div>
+       <Head>
+          <title>
+            reactの練習
+          </title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
     <div>かきくけこ</div>
+    </div>
+   
   )
 }
