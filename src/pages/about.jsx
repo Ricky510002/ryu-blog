7f340ae3c4ac7427d9fa51b458375fcdfee4d8c8
Ryuki Goto <rth510ryu@gmail.com>
2021-05-01 09:24:26 +0900

    リファクタリング

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -7,7 +7,7 @@ export default function about() {
         <title>aboutページ</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>f</div>
+      <div>g</div>
     </div>
   );
 }
