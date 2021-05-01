6695cae0c64eefc342d4da4042a3c5010fac4867
Ryuki Goto <rth510ryu@gmail.com>
2021-05-01 09:23:12 +0900

    表示の変更

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -7,7 +7,7 @@ export default function about() {
         <title>aboutページ</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>a</div>
+      <div>b</div>
     </div>
   );
 }
