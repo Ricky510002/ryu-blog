35dc3d0921ac83c76470e199d224989fe9ea32f3
Ryuki Goto <rth510ryu@gmail.com>
2021-05-05 11:04:33 +0900

    リファクタリング

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>reactの練習</title>
+        <title>a</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
