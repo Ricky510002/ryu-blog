e72e964ef8e2284b99619b00bb86d8ae022e8772
Ryuki Goto <rth510ryu@gmail.com>
2021-05-13 10:54:14 +0900

    Homeページの修正

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,8 @@ import Head from "next/head";
 import styles from "../styles/home.module.css";
 import Link from "next/link";
 
+import Header from "../components/header"
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -9,6 +11,8 @@ export default function Home() {
         <title>RyuBlog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      
+      <Header/>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
@@ -26,16 +30,7 @@ export default function Home() {
         <a>travelページ</a>
       </Link>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      
     </div>
   );
 }
