27479a109a795120aef6f19ad39c5a6b7ff4423b
Ryuki Goto <rth510ryu@gmail.com>
2021-05-15 09:02:28 +0900

    デザイン変更

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,14 +1,21 @@
 import Head from "next/head";
 import styles from "../styles/contact.module.css";
+import Header from "../components/header"
+import Background from "../components/background"
+import Scroll from "../components/scroll"
 
 export default function about() {
   return (
-    <div>
+    <>
       <Head>
         <title>contactページ</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Header/>
+      <Background title="contact" />
+      <Scroll/>
+    
       <div className={styles.about}>contactページです</div>
-    </div>
+    </>
   );
 }
