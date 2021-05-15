d2cfc083c280ec8445269df9cf50f2d0ee4d026f
Ryuki Goto <rth510ryu@gmail.com>
2021-05-15 09:01:23 +0900

    scrollコンポーネントの追加

---
+++ src/components/scroll.jsx
@@ -0,0 +1,9 @@
+import styles from "./scroll.module.css"
+
+export default function Scroll(){
+  return(
+    <div className={styles.scroll}>
+      <span className={styles.txt}></span>
+    </div>
+  );
+}
