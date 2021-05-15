7efe9aec1fe92df02510aea8e581688c9b4f8b63
Ryuki Goto <rth510ryu@gmail.com>
2021-05-15 09:02:19 +0900

    デザイン変更

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,14 +1,25 @@
 import Head from "next/head";
 import styles from "../styles/about.module.css";
+import Header from "../components/header"
+import Background from "../components/background";
+import Scroll from "../components/scroll";
+
+
 
 export default function about() {
   return (
-    <div>
+    <>
       <Head>
         <title>aboutページ</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Header/>
+
+      <Background title="About Me"/>
+      <Scroll />
+
+
       <div className={styles.about}>aboutページです</div>
-    </div>
+    </>
   );
 }
