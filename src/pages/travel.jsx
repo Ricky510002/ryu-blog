a3f365837ca2d9e1265b8795240276437e4cf62e
Ryuki Goto <rth510ryu@gmail.com>
2021-05-15 09:03:04 +0900

    デザイン変更

--- src/pages/travel.jsx
+++ src/pages/travel.jsx
@@ -1,15 +1,23 @@
 import Head from "next/head";
 import styles from "../styles/travel.module.css";
+import Header from "../components/header"
+import Background from "../components/background"
+import Scroll from "../components/scroll"
+
 
 export default function about() {
   return (
-    <div>
+    <>
       <Head>
         <title>travelページ</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Header/>
+
+      <Background title="Travel"/>
+      <Scroll />
       
       <div className={styles.about}>travelページです</div>
-    </div>
+    </>
   );
 }
