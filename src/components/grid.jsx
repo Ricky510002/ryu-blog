410db4128f39dfd17d3bebd5f86315b26f8d2cec
Ryuki Goto <rth510ryu@gmail.com>
2021-05-18 22:53:01 +0900

    gridコンポーネントの作成

---
+++ src/components/grid.jsx
@@ -0,0 +1,127 @@
+import styles from "./grid.module.css";
+
+export default function Grid() {
+  return (
+    <>
+      <div className={styles.body}>
+        <div className={styles.supportGrid}></div>
+
+        <section className={styles.grid1}>
+          <div className={`${styles.panel} ${styles.panelTitle}`}>
+            <h1 className={styles.h1}>Japan</h1>
+            {/* <p className={styles.p}>もしタイトルの下に何か書きたかったらここ</p> */}
+          </div>
+          <div className={`${styles.panel} ${styles.panel1_1}`}></div>
+          <div className={`${styles.panel} ${styles.panel1_2}`}></div>
+          <div className={`${styles.panel} ${styles.panel1_3}`}></div>
+          <div className={`${styles.panel} ${styles.panel1_4}`}></div>
+          <div className={`${styles.panel} ${styles.panel1_5}`}></div>
+          <div className={`${styles.panel} ${styles.panel1_6}`}></div>
+          <div className={`${styles.panel} ${styles.panel1_7}`}></div>
+          <div className={`${styles.panel} ${styles.panel1_8}`}></div>
+          <div className={`${styles.panel} ${styles.panel1_9}`}></div>
+        </section>
+      </div>
+
+      <div className={styles.body}>
+        <div className={styles.supportGrid}></div>
+
+        <section className={styles.grid1}>
+          <div className={`${styles.panel} ${styles.panelTitle}`}>
+            <h1 className={styles.h1}>UK</h1>
+            {/* <p className={styles.p}>もしタイトルの下に何か書きたかったらここ</p> */}
+          </div>
+          <div className={`${styles.panel} ${styles.panel2_1}`}></div>
+          <div className={`${styles.panel} ${styles.panel2_2}`}></div>
+          <div className={`${styles.panel} ${styles.panel2_3}`}></div>
+          <div className={`${styles.panel} ${styles.panel2_4}`}></div>
+          <div className={`${styles.panel} ${styles.panel2_5}`}></div>
+          <div className={`${styles.panel} ${styles.panel2_6}`}></div>
+          <div className={`${styles.panel} ${styles.panel2_7}`}></div>
+          <div className={`${styles.panel} ${styles.panel2_8}`}></div>
+          <div className={`${styles.panel} ${styles.panel2_9}`}></div>
+        </section>
+      </div>
+
+      <div className={styles.body}>
+        <div className={styles.supportGrid}></div>
+
+        <section className={styles.grid1}>
+          <div className={`${styles.panel} ${styles.panelTitle}`}>
+            <h1 className={styles.h1}>Frace</h1>
+            {/* <p className={styles.p}>もしタイトルの下に何か書きたかったらここ</p> */}
+          </div>
+          <div className={`${styles.panel} ${styles.panel3_1}`}></div>
+          <div className={`${styles.panel} ${styles.panel3_2}`}></div>
+          <div className={`${styles.panel} ${styles.panel3_3}`}></div>
+          <div className={`${styles.panel} ${styles.panel3_4}`}></div>
+          <div className={`${styles.panel} ${styles.panel3_5}`}></div>
+          <div className={`${styles.panel} ${styles.panel3_6}`}></div>
+          <div className={`${styles.panel} ${styles.panel3_7}`}></div>
+          <div className={`${styles.panel} ${styles.panel3_8}`}></div>
+          <div className={`${styles.panel} ${styles.panel3_9}`}></div>
+        </section>
+      </div>
+
+      <div className={styles.body}>
+        <div className={styles.supportGrid}></div>
+
+        <section className={styles.grid1}>
+          <div className={`${styles.panel} ${styles.panelTitle}`}>
+            <h1 className={styles.h1}>Germany</h1>
+            {/* <p className={styles.p}>もしタイトルの下に何か書きたかったらここ</p> */}
+          </div>
+          <div className={`${styles.panel} ${styles.panel4_1}`}></div>
+          <div className={`${styles.panel} ${styles.panel4_2}`}></div>
+          <div className={`${styles.panel} ${styles.panel4_3}`}></div>
+          <div className={`${styles.panel} ${styles.panel4_4}`}></div>
+          <div className={`${styles.panel} ${styles.panel4_5}`}></div>
+          <div className={`${styles.panel} ${styles.panel4_6}`}></div>
+          <div className={`${styles.panel} ${styles.panel4_7}`}></div>
+          <div className={`${styles.panel} ${styles.panel4_8}`}></div>
+          <div className={`${styles.panel} ${styles.panel4_9}`}></div>
+        </section>
+      </div>
+
+      <div className={styles.body}>
+        <div className={styles.supportGrid}></div>
+
+        <section className={styles.grid1}>
+          <div className={`${styles.panel} ${styles.panelTitle}`}>
+            <h1 className={styles.h1}>Serbia</h1>
+            {/* <p className={styles.p}>もしタイトルの下に何か書きたかったらここ</p> */}
+          </div>
+          <div className={`${styles.panel} ${styles.panel5_1}`}></div>
+          <div className={`${styles.panel} ${styles.panel5_2}`}></div>
+          <div className={`${styles.panel} ${styles.panel5_3}`}></div>
+          <div className={`${styles.panel} ${styles.panel5_4}`}></div>
+          <div className={`${styles.panel} ${styles.panel5_5}`}></div>
+          <div className={`${styles.panel} ${styles.panel5_6}`}></div>
+          <div className={`${styles.panel} ${styles.panel5_7}`}></div>
+          <div className={`${styles.panel} ${styles.panel5_8}`}></div>
+          <div className={`${styles.panel} ${styles.panel5_9}`}></div>
+        </section>
+      </div>
+
+      <div className={styles.body}>
+        <div className={styles.supportGrid}></div>
+
+        <section className={styles.grid1}>
+          <div className={`${styles.panel} ${styles.panelTitle}`}>
+            <h1 className={styles.h1}>Philippines</h1>
+            {/* <p className={styles.p}>もしタイトルの下に何か書きたかったらここ</p> */}
+          </div>
+          <div className={`${styles.panel} ${styles.panel6_1}`}></div>
+          <div className={`${styles.panel} ${styles.panel6_2}`}></div>
+          <div className={`${styles.panel} ${styles.panel6_3}`}></div>
+          <div className={`${styles.panel} ${styles.panel6_4}`}></div>
+          <div className={`${styles.panel} ${styles.panel6_5}`}></div>
+          <div className={`${styles.panel} ${styles.panel6_6}`}></div>
+          <div className={`${styles.panel} ${styles.panel6_7}`}></div>
+          <div className={`${styles.panel} ${styles.panel6_8}`}></div>
+          <div className={`${styles.panel} ${styles.panel6_9}`}></div>
+        </section>
+      </div>
+    </>
+  );
+}
