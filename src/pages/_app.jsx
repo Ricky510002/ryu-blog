a331e8c2420548c412f1744ceed15341899e8608
Ryuki Goto <rth510ryu@gmail.com>
2021-05-19 22:54:36 +0900

    css修正

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,11 +1,19 @@
-import "../styles/globals.css";
+// import "../styles/globals.css";
+// import Router from 'next/router'
+// import * as gtag from '../lib/gtag'
+
+
+// function MyApp({ Component, pageProps }) {
+//   Router.events.on('routeChangeComplete', url => gtag.pageview(url))
+//   return <Component {...pageProps} />;
+// }
+
+// export default MyApp;
+import App from 'next/app'
 import Router from 'next/router'
+
 import * as gtag from '../lib/gtag'
 
+Router.events.on('routeChangeComplete', url => gtag.pageview(url))
 
-function MyApp({ Component, pageProps }) {
-  Router.events.on('routeChangeComplete', url => gtag.pageview(url))
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
+export default App
