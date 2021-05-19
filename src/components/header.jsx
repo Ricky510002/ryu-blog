77a7bacf664231f864c68228d85a3577ab1d9f3d
Ryuki Goto <rth510ryu@gmail.com>
2021-05-19 13:16:54 +0900

    ロゴの変更

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,6 @@
 import styles from "./header.module.css"
 import Burger from "./burger"
+import Link from "next/link";
 
 
 
@@ -8,9 +9,12 @@ export default function Header(){
   return(
     <>
       <div className={styles.nav}>
-        <div className={styles.logo}>
-          LOGO
-        </div>
+        <Link href="/">
+          <div className={styles.logo}>
+            <img src="150511 (1).png" alt="ロゴ" className={styles.img}/>
+          </div>
+        </Link>
+        
         <Burger/>
         
       </div>
