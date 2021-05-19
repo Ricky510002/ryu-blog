8d9596c5e80c9cb5dcbe6e191660ecbbbeb60101
Ryuki Goto <rth510ryu@gmail.com>
2021-05-19 14:53:36 +0900

    スマホ背景修正

--- src/components/background.jsx
+++ src/components/background.jsx
@@ -3,8 +3,11 @@ import styles from "./background.module.css";
 export default function Background(props){
   return(
     <>
-    <div className={styles.background}>
-      <div className={styles.text}>{props.title}</div>
+    <div className={styles.backgroundWrapper}>
+      <div className={styles.background}>
+        <div className={styles.text}>{props.title}</div>
+      </div>
+
     </div>
     </>
   );
