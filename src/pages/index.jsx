63ccda9408c9d216f9d00a01f10d8025aa4ee789
Ryuki Goto <rth510ryu@gmail.com>
2021-05-22 15:05:58 +0900

    リファクタリング

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -35,7 +35,9 @@ export default function Home({ blog, totalCount }) {
                       alt="ブログのサムネイル"
                     ></img>
                     <div className={styles.card_content_06}>
-                      <p className={styles.category}>{blog.category && `${blog.category.name}`}</p>
+                      <p className={styles.category}>
+                        {blog.category && `${blog.category.name}`}
+                      </p>
                       <p className={styles.card_title_06}>{blog.title}</p>
                       <div
                         className={styles.card_text_06}
